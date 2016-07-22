9a16643babac553aa877f775f4be832ee9270c14
Jeremy Toce <jeremytoce@gmail.com>
2016-07-21 19:23:28 -0700

    Implemented Cron scheduler

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ var Twitter = require('twitter');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var cron = require('node-cron');
+var exec = require('child_process').exec;
+
 
 var app = express();
 
@@ -18,4 +21,29 @@ app.use(express.static('DashboardClient'));
 app.listen(3000, function (req, res) {
 	console.log('server is listening on 3000');
 });
+
+//////
+
+var task = cron.schedule('* * * * *', function() {
+  console.log('Server is still running');
+}, false);
+
+
+var test = cron.schedule('* * * * *', function() {
+  console.log('Update CSV running is still running');
+  exec(__dirname + '/updateCSV.sh', function(error, stdout, stderr) {
+      console.log('stdout: ', stdout);
+      console.log('stderr: ', stderr);
+      if (error !== null) {
+          console.log('exec error: ', error);
+      }
+  });
+}, false);
+
+
+
+
+task.start();
+test.start();
+
 
