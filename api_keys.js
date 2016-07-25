e647d8c8e003ac84afcf9b37a180042fa859ea22
n-white <neil.r.whitehead@gmail.com>
2016-07-25 16:00:53 -0700

    api file reset

--- api_keys.js
+++ api_keys.js
@@ -1,1 +1,10 @@
-//empty
+module.exports = {
+ consumer_key: 'pDjRQtWidCnjJPx77cvcxqa02',
+ consumer_secret: '7gphxKFTNdnE8E90MGDghMobNodUUoWexXMMDEccGVZDS3GgTE',
+ access_token_key: '753453006058758144-JIevdY4PILsPQ9Cc0ZbABoLo0PXGzAr',
+ access_token_secret: 'zYvEocaXhZWwAUspHr95hlfvSwiEx0lJZQ2hcqgZlW46I',
+ watson_api_key: '51ee6e603c9429b478cc0b81caa7f7068b6211e6'
+}
+
+// neil: 8b2fff3df62c098999daf886ee2d7f5aeccb9689
+// jordan: 51ee6e603c9429b478cc0b81caa7f7068b6211e6
